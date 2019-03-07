68001f27681aa7d068d0b597896f0174eae400c7
Wessel van Ree <wesselvanree@gmail.com>
2019-03-07 22:16:00 +0100

    finished NewExercise.java

--- NewExercise.java
+++ NewExercise.java
@@ -20,15 +20,29 @@ public class NewExercise
     System.out.print("What would you like to name the file? ");
     String file = input.next();
 
+    // create directory
     String newDir = pwd + "/Exercise" + num;
     File dir = new File(newDir);
     boolean dstatus = dir.mkdir();
 
+    // create file
     String filePath = newDir + "/" + file + ".java";
     File f = new File(filePath);
     boolean fstatus = f.createNewFile();
 
+    // outputs status
     System.out.println("\nCreated directory: " + dstatus);
     System.out.println("Created file: " + fstatus);
+
+    // write inside file
+    try {
+      BufferedWriter bw = new BufferedWriter(new FileWriter(filePath));
+      String text = "public class " + file + "\n{\n  public static void main(String[] args)\n  {\n    \n  }\n}";
+      bw.write(text);
+      bw.close();
+      System.out.println("Wrote inside file: true");
+    } catch (IOException ex){
+      System.out.println("couldn't write inside file: " + ex);
+    }
   }
 }
