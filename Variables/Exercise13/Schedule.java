8ac3b1899be6d3f153bc7af76d195a4cd0ef45cd
Wessel van Ree <wesselvanree@gmail.com>
2019-03-07 18:01:22 +0100

    update

--- Variables/Exercise13/Schedule.java
+++ Variables/Exercise13/Schedule.java
@@ -1,25 +1,25 @@
 public class Schedule {
   public static void main(String[] args)
   {
-    // s for subject, t for teacher
-    String s1, s2, s3, s4, s5, s6, s7, s8, t1, t2, t3, t4, t5, t6, t7, t8;
-    s1 = "English III"; t1 = "Ms. Lapan";
-    s2 = "Precalculus"; t2 = "Mrs. Gideon";
-    s3 = "Music Theory"; t3 = "Mr. Davis";
-    s4 = "Biotechnology"; t4 = "Ms. Palmer";
-    s5 = " Principles of Technology I"; t5 = "Ms. Garcia";
-    s6 = "Latin II"; t6 = "Mrs. Barnett";
-    s7 = "AP US History"; t7 = "Ms. Johannessen";
-    s8 = "Business Computer Infomation Systems"; t8 = "Mr. James";
+    // c for course, t for teacher
+    String c1, c2, c3, c4, c5, c6, c7, c8, t1, t2, t3, t4, t5, t6, t7, t8;
+    c1 = "English III"; t1 = "Ms. Lapan";
+    c2 = "Precalculus"; t2 = "Mrs. Gideon";
+    c3 = "Music Theory"; t3 = "Mr. Davis";
+    c4 = "Biotechnology"; t4 = "Ms. Palmer";
+    c5 = " Principles of Technology I"; t5 = "Ms. Garcia";
+    c6 = "Latin II"; t6 = "Mrs. Barnett";
+    c7 = "AP US History"; t7 = "Ms. Johannessen";
+    c8 = "Business Computer Infomation Systems"; t8 = "Mr. James";
     System.out.println("+------------------------------------------------------------+");
-    System.out.println("| 1 |                          " + s1 +" |       " + t1 + " |");
-    System.out.println("| 2 |                          " + s2 + " |     " + t2 + " |");
-    System.out.println("| 3 |                         " + s3 + " |       " + t3 + " |");
-    System.out.println("| 4 |                        " + s4 + " |      " + t4 + " |");
-    System.out.println("| 5 |          " + s5 + " |      " + t5 + " |");
-    System.out.println("| 6 |                             " + s6 + " |    " + t6 + " |");
-    System.out.println("| 7 |                        " + s7 + " | " + t7 + " |");
-    System.out.println("| 8 | " + s8 + " |       " + t8 + " |");
+    System.out.println("| 1 |                          " + c1 +" |       " + t1 + " |");
+    System.out.println("| 2 |                          " + c2 + " |     " + t2 + " |");
+    System.out.println("| 3 |                         " + c3 + " |       " + t3 + " |");
+    System.out.println("| 4 |                        " + c4 + " |      " + t4 + " |");
+    System.out.println("| 5 |          " + c5 + " |      " + t5 + " |");
+    System.out.println("| 6 |                             " + c6 + " |    " + t6 + " |");
+    System.out.println("| 7 |                        " + c7 + " | " + t7 + " |");
+    System.out.println("| 8 | " + c8 + " |       " + t8 + " |");
     System.out.println("+------------------------------------------------------------+");
 
   }
