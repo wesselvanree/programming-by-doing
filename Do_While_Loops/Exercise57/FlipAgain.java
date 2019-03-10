ea782b23fdf196ad52eb79573ba318de8886238c
Wessel van Ree <wesselvanree@gmail.com>
2019-03-10 09:12:37 +0100

    exercise 57, 2 en 3

--- Do_While_Loops/Exercise57/FlipAgain.java
+++ Do_While_Loops/Exercise57/FlipAgain.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 
+// 1. verander de standaardvalue naar 'y', dit zorgt ervoor dat de while loop de eerste keer altijd werkt
+
 public class FlipAgain
 {
 	public static void main( String[] args )
@@ -8,12 +10,13 @@ public class FlipAgain
 		Scanner keyboard = new Scanner(System.in);
 		Random rng = new Random();
 
-    // nu is de standaardvalue 'y' en dit zorgt ervoor dat de while loop de eerste keer altijd werkt
-		String again = "y";
+    // geen startvalue meer nodig omdat de do-while loop eerst 1 keer uitvoert en daarna kijkt of de while statement true is en als dat zo is voert die nog een keer uit
+    String again;
 
-		while ( again.equals("y") )
-		{
-			int flip = rng.nextInt(2);
+    // voert de code tussen de curly braces eerst uit voordat de while statement gecontroleerd wordt
+    do
+    {
+      int flip = rng.nextInt(2);
 			String coin;
 
 			if ( flip == 1 )
@@ -25,6 +28,7 @@ public class FlipAgain
 
 			System.out.print( "Would you like to flip again (y/n)? " );
 			again = keyboard.next();
-		}
+    } while ( again.equals("y") );
+
 	}
 }
