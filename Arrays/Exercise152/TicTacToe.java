75ea0d10ec9050aea79534e6f19a488f6dc98512
Wessel van Ree <wesselvanree@gmail.com>
2019-03-14 17:45:26 +0100

    update

--- Arrays/Exercise152/TicTacToe.java
+++ Arrays/Exercise152/TicTacToe.java
@@ -22,7 +22,7 @@ public class TicTacToe
 		column = keyboard.nextInt();
 		board[row][column] = currentPlayer;
 
-		while (!won || !tie) {
+		while (!won && !tie) {
 			if (currentPlayer == 'O')
 			{
 				currentPlayer = 'X';
@@ -39,16 +39,8 @@ public class TicTacToe
 
 			board[row][column] = currentPlayer;
 
-			if (winGame('X'))
-			{
-				won = true;
-			}
-			else if (winGame('O'))
-			{
-				won = true;
-			}
-
 			won = checkWin();
+			tie = checkTie();
 		}
 
 		System.out.println("\nEnd of game");
@@ -93,31 +85,28 @@ public class TicTacToe
 		return result;
 	}
 
-	public static boolean winGame( char player )
+	public static boolean checkTie()
 	{
-		boolean result = false;
-		// check horizontal
-		for ( int r = 0; r < 3; r++ )
+		System.out.println("checking tie...");
+		boolean[] b = new boolean[3];
+
+		for (int r = 0; r < 3; r++)
 		{
-			if ( player == board[r][0] && board[r][0] == board[r][1] && board[r][1] == board[r][2] )
-				result = true;
+			if (board[r][0] != ' ' && board[r][1] != ' ' && board[r][2] != ' ')
+			{
+				b[r] = true;
+			}
 		}
 
-		// check vertical
-		for ( int c = 0; c < 3; c++ )
+		if (b[0] && b[1] && b[2])
 		{
-			if ( player == board[0][c] && board[0][c] == board[1][c] && board[1][c] == board[2][c] )
-				result = true;
+			return true;
+		}
+		else
+		{
+			return false;
 		}
 
-		// check diagonal
-		if ( player == board[0][0] && board[0][0] == board[1][1] && board[1][1] == board[2][2] )
-				result = true;
-		else if ( player == board[0][2] && board[0][2] == board[1][1] && board[1][1] == board[2][0] )
-				result = true;
-
-
-		return result;
 	}
 
 
